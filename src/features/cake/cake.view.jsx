52c9f5011959361493520d30aa41ab0c2970c291
Ni.Bagherpour <ni.bagherpour@sadad.co.ir>
2023-09-07 13:34:15 +0330

    feat: useDispatch hook

--- src/features/cake/cake.view.jsx
+++ src/features/cake/cake.view.jsx
@@ -1,14 +1,15 @@
-import { useSelector } from "react-redux";
-
+import { useDispatch, useSelector } from "react-redux";
+import {ordered , restocked} from "./cake-slice"
 export const CakeView = () => {
 
   const numOfCakes = useSelector((state) => state.cake.numOfCakes);
-  
+  const dispatch = useDispatch();
+
   return (
     <div>
       <h2>Number of cakes - {numOfCakes}</h2>
-      <button>Order cake</button>
-      <button>Restock cake</button>
+      <button onClick={() => dispatch(ordered())}>Order Cake</button>
+      <button onClick={() => dispatch(restocked(5))}>Restock Cakes</button>
     </div>
   );
 };
--- src/features/icecream/icecream.view.jsx
+++ src/features/icecream/icecream.view.jsx
@@ -1,13 +1,24 @@
-import { useSelector } from "react-redux";
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { ordered, restocked } from "./icecream-slice";
 
 export const IcecreamView = () => {
   const numOfIcecreams = useSelector((state) => state.icecream.numOfIcecreams);
+  const dispatch = useDispatch();
+  const [value, setValue] = useState(1);
 
   return (
     <div>
       <h2>Number of ice creams - {numOfIcecreams}</h2>
-      <button>Order ice cream</button>
-      <button>Restock ice cream</button>
+      <button onClick={() => dispatch(ordered())}>Order Ice cream</button>
+      <input
+        type="number"
+        value={value}
+        onChange={(e) => setValue(parseInt(e.target.value))}
+      />
+      <button onClick={() => dispatch(restocked(value))}>
+        Restock Ice creams
+      </button>
     </div>
   );
 };
